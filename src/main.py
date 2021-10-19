07b5e367df2188fb822672375a8e7e43729dd605
Ben Homan <ben@benhoman.com>
2021-10-19 04:31:34 -0400

    added in base Event model.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,37 @@
+from datetime import datetime
 from fastapi import FastAPI
-from sqlmodel import Field, SQLModel, create_engine
-from typing import Optional
-
-app = FastAPI()
+from sqlmodel import Field, SQLModel, create_engine, Relationship, Column, JSON
+from typing import Optional, Any
 
 
 class UserSession(SQLModel, table=True):
     id: Optional[str] = Field(default=None, primary_key=True)
 
 
+class EventBase(SQLModel):
+    category: str
+    name: str
+    data: dict[Any, Any] = Field(
+        index=False,
+        sa_column=Column(JSON)
+    )
+    timestamp: datetime
+    session_id: Optional[str] = Field(
+        default=None,
+        foreign_key="usersession.id"
+    )
+
+
+class Event(EventBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    session: Optional[UserSession] = Relationship(back_populates="events")
+
+
 sqlite_url = "sqlite:///database.db"
 engine = create_engine(sqlite_url)
+
+
+app = FastAPI()
 
 
 def create_db_and_tables():
